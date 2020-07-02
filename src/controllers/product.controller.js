38a2d2007ab59ee63e806a63fcf2fe1f722f72ed
cartory <xXcartoryXx@gmail.com>
2020-07-01 22:31:37 -0400

    promo deleted

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,34 @@
+const { Product } = require("../models/Product");
+
+class ProductController {
+  static async all(req, res) {
+    res.json(await Product.instance.all());
+  }
+
+  static async find(req, res) {
+    res.json(await Product.instance.find(req.params.id));
+  }
+
+  static async store(req, res) {
+    res.json(await Product.instance.create(req.body));
+  }
+
+  static async update(req, res) {
+    res.json(await Product.instance.update(req.params.id, req.body));
+  }
+
+  static async destroy(req, res) {
+    res.json(await Product.instance.destroy(req.params.id));
+  }
+
+  static async getByPromo(req, res) {
+    res.json(await Product.instance.getByPromo());
+  }
+
+  static async getByCategory(req, res) {
+    console.log("DEBUG");
+    res.json(await Product.instance.getByCategory(req.params.cat));
+  }
+}
+
+module.exports = { ProductController };
