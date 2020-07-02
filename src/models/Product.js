219fade6f62f5485971822231c9e76437e6a0b8f
cartory <xXcartoryXx@gmail.com>
2020-07-01 22:29:10 -0400

    query byCategories

--- src/models/Product.js
+++ src/models/Product.js
@@ -10,17 +10,30 @@ class Product extends Model {
 
   async getByPromo() {
     var p = [];
-    this.db.once("value").then(
-        function(snapshot){
-            snapshot.forEach((product)=>{
-                p.push(product.child("promo").val());
-            })
-        }
-    );
+    this.db.once("value").then(function (snapshot) {
+      snapshot.forEach((product) => {
+        p.push(product.child("promo").val());
+      });
+    });
 
     return p;
   }
 
+  async getByCategory(categoryID) {
+    var products = await this.db.once("value");
+    var array = [];
+    // console.log(products);
+    products.forEach((product) => {
+      product.child("category").forEach((item) => {;
+        if (item.val() == categoryID) {
+          console.log("match!!!");
+          array.push(product.val());
+        }
+      });
+    });
+    console.log(array);
+    return array;
+  }
 }
 
 module.exports = { Product };
