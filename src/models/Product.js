bc657caef6961e482f3c6dbc82ad28053910b3c3
cartory <xXcartoryXx@gmail.com>
2020-07-15 16:27:08 -0400

    withID

--- src/models/Product.js
+++ src/models/Product.js
@@ -12,10 +12,12 @@ class Product extends Model {
     var products = await this.db.once("value");
     var array = [];
     products.forEach((product) => {
+      var p = product.val();
+      p.id = product.key;
       let d = new Date(product.child("promo").child("endDate").val());
       let t = new Date();
       if (t <= d) {
-        array.push(product);
+        array.push(p);
       }
     });
 
@@ -26,14 +28,14 @@ class Product extends Model {
     var products = await this.db.once("value");
     var array = [];
     products.forEach((product) => {
+      var p = product.val();
+      p.id = product.key;
       product.child("category").forEach((item) => {
         if (item.val() == categoryID) {
-          console.log("match!!!");
-          array.push(product.val());
+          array.push(p);
         }
       });
     });
-    console.log(array);
     return array;
   }
 
@@ -41,13 +43,14 @@ class Product extends Model {
     var products = await this.db.once("value");
     var array = [];
     products.forEach((product) => {
-      product.ref.parent.once('value');
-      if (product.child("name").val().toLowerCase().includes(name)) {
-        array.push(product.val());
+      var p = product.val();
+      p.id = product.key;
+      if (product.child("name").val().toLowerCase().includes(name.toLowerCase())) {
+        array.push(p);
       } else if (
-        product.child("description").val().toLowerCase().includes(name)
+        product.child("description").val().toLowerCase().includes(name.toLowerCase())
       ) {
-        array.push(product.val());
+        array.push(p);
       }
     });
     return array;
